85d6ee76d38e917025cc8e3923c3da73ae2a7969
vikas.kumar <vikas.kumar@robosoftin.com>
2021-08-25 14:32:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn react is Super Awesome!! Click here!
         </a>
       </header>
     </div>
